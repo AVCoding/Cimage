c7bb6dc83c21d6e4a2293269581782c0531d541a
AVCoding <70842251+AVCoding@users.noreply.github.com>
2022-05-19 17:00:57 +0300

    w

---
+++ script.js
@@ -0,0 +1,30 @@
+    window.onload = async function() { 
+
+    	// copy info from functions.js
+    	var info = {
+		    'env': asmLibraryArg,
+		    'wasi_snapshot_preview1': asmLibraryArg,
+		  };
+
+		let wasmMemory =  new WebAssembly.Memory({ initial: 256, maximum: 256}); // Wasm memory 
+
+		let wasmExports = null;
+     	let response = await fetch('functions.wasm');
+        let bytes = await response.arrayBuffer(); //convert file into a byte array
+        let wasmObj = await WebAssembly.instantiate(bytes, info); //create a new WebAssembly object
+        wasmExports = wasmObj.instance.exports; //exported functions
+        console.log(wasmExports);
+
+        const { length } = imageData;
+        const memory = _malloc(length); // Allocating WASM memory
+        HEAPU8.set(imageData, memory); // Copying JS image data to WASM memory
+        _grayScale(memory, length); // Calling WASM method
+        const filteredImageData = HEAPU8.subarray(memory, memory + length); // Converting WASM data to JS Image data
+        _free(memory); // Freeing WASM memory
+        return filteredImageData;
+
+        document.getElementById('getSqrbtn').addEventListener("click", function(){
+        	document.getElementById('result').innerText =  _getSqr( document.getElementById('sqrinput').value ); 
+        });
+
+    }
