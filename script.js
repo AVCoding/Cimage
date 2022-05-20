edaf837fcfdc66974c372d20f757d610f9e5593e
AVCoding <70842251+AVCoding@users.noreply.github.com>
2022-05-20 12:28:18 +0300

    dd

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
     window.onload = async function() { 
 
-	// copy asmLibraryArg from functions.js
-	var asmLibraryArg = {};
+// 	// copy asmLibraryArg from functions.js
+// 	var asmLibraryArg = {};
 	    
-    	// copy info from functions.js
-    	var info = {
-		    'env': asmLibraryArg,
-		    'wasi_snapshot_preview1': asmLibraryArg,
-		  };
+//     	// copy info from functions.js
+//     	var info = {
+// 		    'env': asmLibraryArg,
+// 		    'wasi_snapshot_preview1': asmLibraryArg,
+// 		  };
 
-		let wasmMemory =  new WebAssembly.Memory({ initial: 256, maximum: 256}); // Wasm memory 
+// 		let wasmMemory =  new WebAssembly.Memory({ initial: 256, maximum: 256}); // Wasm memory 
 
-		let wasmExports = null;
-     	let response = await fetch('functions.wasm');
-        let bytes = await response.arrayBuffer(); //convert file into a byte array
-        let wasmObj = await WebAssembly.instantiate(bytes, info); //create a new WebAssembly object
-        wasmExports = wasmObj.instance.exports; //exported functions
-        console.log(wasmExports);
+// 		let wasmExports = null;
+//      	let response = await fetch('functions.wasm');
+//         let bytes = await response.arrayBuffer(); //convert file into a byte array
+//         let wasmObj = await WebAssembly.instantiate(bytes, info); //create a new WebAssembly object
+//         wasmExports = wasmObj.instance.exports; //exported functions
+//         console.log(wasmExports);
 
 
 
